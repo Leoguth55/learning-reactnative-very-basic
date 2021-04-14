0ebad3aa664aaad70c5b0384765e4871f5ce8d0c
Leonardo Guth <leoguth55@hotmail.com>
2021-04-14 11:31:58 +0100

    some updates

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,75 +1,79 @@
 import React, { useEffect, useState } from 'react';
 import {
-    SafeAreaView,
-    FlatList,
-    View,
-    Text,
-    StyleSheet,
-    StatusBar,
-    Button,
-    TextInput,
+  SafeAreaView,
+  FlatList,
+  View,
+  Text,
+  StyleSheet,
+  StatusBar,
+  Button,
+  TextInput,
 } from 'react-native';
 import api from '../services/api';
 
 export default function Home() {
-    const [projects, setProjects] = useState([]);
+  const [projects, setProjects] = useState([]);
 
-    useEffect(() => {
-        api.get('projects')
-            .then((response) => {
-                setProjects(response.data);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    }, []);
+  useEffect(() => {
+    api
+      .get('projects')
+      .then((response) => {
+        setProjects(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-    function postProject() {
-        api.post('projects', {
-            title: '4',
-            owner: 'leo',
-        }).then((response) => {
-            setProjects([...projects, response.data]);
-        });
-    }
+  function postProject() {
+    api
+      .post('projects', {
+        title: 'project',
+        owner: 'leo',
+      })
+      .then((response) => {
+        setProjects([...projects, response.data]);
+      });
+  }
 
-    function deleteProject(id) {
-        api.delete(`projects/${id}`).catch((err) => {
-            console.log(err);
-        });
-        setProjects(projects.filter((project) => project.id !== id));
-    }
+  function deleteProject(id) {
+    api.delete(`projects/${id}`).catch((err) => {
+      console.log(err);
+    });
+    setProjects(projects.filter((project) => project.id !== id));
+  }
 
-    return (
-        <>
-            <StatusBar barStyle="dark-content" />
-            <Button title="Learn More" onPress={postProject} />
-            <SafeAreaView style={styles.container}>
-                <FlatList
-                    data={projects}
-                    keyExtractor={(project) => project.id}
-                    renderItem={({ item: project }) => (
-                        <Text
-                            key={project.id}
-                            style={styles.title}
-                            onPress={() => deleteProject(project.id)}
-                        >
-                            {project.id}
-                        </Text>
-                    )}
-                />
-            </SafeAreaView>
-        </>
-    );
+  return (
+    <>
+      <StatusBar barStyle='dark-content' />
+      <Button title='Add Proj' onPress={postProject} />
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={projects}
+          keyExtractor={(project) => project.id}
+          renderItem={({ item: project }) => (
+            <Text
+              key={project.id}
+              style={styles.title}
+              onPress={() => deleteProject(project.id)}>
+              Owner: {project.owner}
+            </Text>
+          )}
+        />
+      </SafeAreaView>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: '#7000c1',
-        flex: 1,
-    },
-    title: {
-        fontSize: 32,
-        fontWeight: 'bold',
-    },
+  container: {
+    backgroundColor: '#00a5a7',
+    flex: 1,
+  },
+  title: {
+    backgroundColor: '#00a599',
+    textAlign: 'center',
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
 });
